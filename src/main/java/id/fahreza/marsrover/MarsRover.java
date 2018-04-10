8e7353bdefa18adf670d3ba7f71f968ccb5f00c7
Harits Fahreza Christyonotoputra <harits.fahreza@gmail.com>
2018-04-10 13:33:51 +0700

    Add MarsRover Current Orientation and Move method
    Add MarsRover current orientation attribute so the client know the last
    orientation of MarsRover.
    Add move method to change orientation of MarsRover.

--- src/main/java/id/fahreza/marsrover/MarsRover.java
+++ src/main/java/id/fahreza/marsrover/MarsRover.java
@@ -1,8 +1,12 @@
 package id.fahreza.marsrover;
 
+import java.util.Arrays;
+
 public class MarsRover {
 
-    private int maximumX = 0, maximumY = 0, currentX = 0, currentY = 0;
+    private final String[] ORIENTATIONS = new String[] { "N", "E", "S", "W" };
+
+    private int maximumX = 0, maximumY = 0, currentX = 0, currentY = 0, currentOrientation = 0;
 
     public MarsRover(int maximumX, int maximumY) {
         this.maximumX = maximumX;
@@ -10,10 +14,19 @@ public class MarsRover {
     }
 
     public MarsRover(int maximumX, int maximumY, int currentX, int currentY) {
-        this.maximumX = maximumX;
-        this.maximumY = maximumY;
+        this(maximumX, maximumY);
         this.currentX = currentX;
         this.currentY = currentY;
+    }
+
+    public MarsRover(int maximumX, int maximumY, String currentOrientation) {
+        this(maximumX, maximumY);
+        this.currentOrientation = Arrays.binarySearch(ORIENTATIONS, currentOrientation);
+    }
+
+    public MarsRover(int maximumX, int maximumY, int currentX, int currentY, String currentOrientation) {
+        this(maximumX, maximumY, currentX, currentY);
+        this.currentOrientation = Arrays.binarySearch(ORIENTATIONS, currentOrientation);
     }
 
     public int getMaximumX() {
@@ -30,6 +43,18 @@ public class MarsRover {
 
     public int getCurrentY() {
         return currentY;
+    }
+
+    public String getCurrentOrientation() {
+        return ORIENTATIONS[currentOrientation];
+    }
+
+    public void move(String command) {
+        if (command.equalsIgnoreCase("R")) {
+            currentOrientation = currentOrientation + 1 > 3 ? 0 : currentOrientation + 1;
+        } else if (command.equalsIgnoreCase("L")) {
+            currentOrientation = currentOrientation - 1 < 0 ? 3 : currentOrientation - 1;
+        }
     }
 
 }
--- src/test/java/id/fahreza/marsrover/MarsRoverTest.java
+++ src/test/java/id/fahreza/marsrover/MarsRoverTest.java
@@ -14,11 +14,27 @@ public class MarsRoverTest {
     }
     
     @Test
-    public void testInitRoverCoordinate_givenRoverCoordinate_shouldCurrentRoverCoordinate() {
+    public void testInitRoverCoordinate_givenRoverCoordinate_shouldReturnCurrentRoverCoordinate() {
         int x = 3, y = 3, roverX = 2, roverY = 2;
         MarsRover marsRover = new MarsRover(x, y, roverX, roverY);
         Assert.assertEquals(roverX, marsRover.getCurrentX());
         Assert.assertEquals(roverY, marsRover.getCurrentY());
+    }
+    
+    @Test
+    public void testChangeRoverOrientation_givenTurnRightCommand_shouldReturnEastOrientation() {
+        int x = 3, y = 3;
+        MarsRover marsRover = new MarsRover(x, y);
+        marsRover.move("R");
+        Assert.assertEquals("E", marsRover.getCurrentOrientation());
+    }
+    
+    @Test
+    public void testChangeRoverOrientation_givenTurnLeftCommand_shouldReturnWestOrientation() {
+        int x = 3, y = 3;
+        MarsRover marsRover = new MarsRover(x, y);
+        marsRover.move("L");
+        Assert.assertEquals("W", marsRover.getCurrentOrientation());
     }
 
 }
