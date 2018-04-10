b3eb6aec25a8e20d39b3a876ad0ccaaca634437f
Harits Fahreza Christyonotoputra <harits.fahreza@gmail.com>
2018-04-10 10:50:05 +0700

    Add MarsRover Current Coordinate Attributes
    Add MarsRover current coordinate attributes so the client can set
    the MarsRover deployed position.

--- src/main/java/id/fahreza/marsrover/MarsRover.java
+++ src/main/java/id/fahreza/marsrover/MarsRover.java
@@ -2,11 +2,18 @@ package id.fahreza.marsrover;
 
 public class MarsRover {
 
-    private int maximumX = 0, maximumY = 0;
+    private int maximumX = 0, maximumY = 0, currentX = 0, currentY = 0;
 
     public MarsRover(int maximumX, int maximumY) {
         this.maximumX = maximumX;
         this.maximumY = maximumY;
+    }
+
+    public MarsRover(int maximumX, int maximumY, int currentX, int currentY) {
+        this.maximumX = maximumX;
+        this.maximumY = maximumY;
+        this.currentX = currentX;
+        this.currentY = currentY;
     }
 
     public int getMaximumX() {
@@ -15,6 +22,14 @@ public class MarsRover {
 
     public int getMaximumY() {
         return maximumY;
+    }
+
+    public int getCurrentX() {
+        return currentX;
+    }
+
+    public int getCurrentY() {
+        return currentY;
     }
 
 }
--- src/test/java/id/fahreza/marsrover/MarsRoverTest.java
+++ src/test/java/id/fahreza/marsrover/MarsRoverTest.java
@@ -12,5 +12,13 @@ public class MarsRoverTest {
         Assert.assertEquals(x, marsRover.getMaximumX());
         Assert.assertEquals(y, marsRover.getMaximumY());
     }
+    
+    @Test
+    public void testInitRoverCoordinate_givenRoverCoordinate_shouldCurrentRoverCoordinate() {
+        int x = 3, y = 3, roverX = 2, roverY = 2;
+        MarsRover marsRover = new MarsRover(x, y, roverX, roverY);
+        Assert.assertEquals(roverX, marsRover.getCurrentX());
+        Assert.assertEquals(roverY, marsRover.getCurrentY());
+    }
 
 }
