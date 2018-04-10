ed67ea67daeec9aeefd9ca487ac98898ab0d64ac
Harits Fahreza Christyonotoputra <harits.fahreza@gmail.com>
2018-04-10 14:56:03 +0700

    Create Main class execution
    Create Main class to execute MarsRover using command-line.

---
+++ src/main/java/id/fahreza/marsrover/Main.java
@@ -0,0 +1,45 @@
+package id.fahreza.marsrover;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int maximumX = 0, maximumY = 0;
+        List<MarsRover> marsRovers = new ArrayList<>();
+
+        if (scanner.hasNextLine()) {
+            String[] lineArray = scanner.nextLine().split(" ");
+            maximumX = new Integer(lineArray[0]);
+            maximumY = new Integer(lineArray[1]);
+        }
+
+        boolean isFinished = false;
+        MarsRover marsRover = null;
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            if (line.isEmpty())
+                break;
+            if (!isFinished) {
+                String[] lineArray = line.split(" ");
+                marsRover = new MarsRover(maximumX, maximumY, new Integer(lineArray[0]), new Integer(lineArray[1]),
+                        lineArray[2]);
+                isFinished = true;
+            } else {
+                marsRover.move(line);
+                marsRovers.add(marsRover);
+                isFinished = false;
+            }
+        }
+
+        scanner.close();
+        for (MarsRover marsRoverResult : marsRovers) {
+            System.out.println(marsRoverResult.getCurrentX() + " " + marsRoverResult.getCurrentY() + " "
+                    + marsRoverResult.getCurrentOrientation());
+        }
+    }
+
+}
